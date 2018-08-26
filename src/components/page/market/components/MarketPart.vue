cc7dbcdd1cae27dca3eb9a6f2aaf34ee1bd53318
pickle_tiger <1062155964@qq.com>
2018-08-26 15:37:00 +0800

    工艺卡 分页

--- src/components/page/market/components/MarketPart.vue
+++ src/components/page/market/components/MarketPart.vue
@@ -1,74 +1,25 @@
 <template>
   <div>
-    <el-container class="project-main">
-      <el-header class="header">部件信息</el-header>
-      <el-main>
-        <div class="main">
-          <el-form ref="form"  label-width="80px">
-            <el-form-item label="部件名称">
-              <el-col :span="10">
-                <el-input ></el-input>
-              </el-col>
-            </el-form-item>
-            <el-form-item label="截止时间">
-              <el-col :span="10">
-                <el-date-picker type="date" placeholder="选择日期" style="width: 100%;"></el-date-picker>
-              </el-col>
-            </el-form-item>
-            <el-form-item label="部件编码">
-              <el-col :span="10">
-                <el-input ></el-input>
-              </el-col>
-            </el-form-item>
-            <el-form-item label="部件进度">
-              <el-collapse accordion>
-                <el-collapse-item title="子部件1">
-                 <el-form>
-                  <el-form-item label="工艺路线1"></el-form-item>
-                    <el-steps :space="100" :active="1" finish-status="success">
-                      <el-step title="完成"></el-step>
-                      <el-step title="建设中"></el-step>
-                      <el-step title="未完成"></el-step>
-                    </el-steps>
-                  <el-form-item label="工艺路线2"></el-form-item>
-                    <el-steps :space="100" :active="0" finish-status="success">
-                      <el-step title="建设中"></el-step>
-                      <el-step title="未完成"></el-step>
-                      <el-step title="未完成"></el-step>
-                    </el-steps>
-                </el-form>
-                </el-collapse-item>
-                <el-collapse-item title="子部件2">
-                 <el-form>
-                  <el-form-item label="工艺路线1"></el-form-item>
-                    <el-steps :space="100" :active="1" finish-status="success">
-                      <el-step title="完成"></el-step>
-                      <el-step title="建设中"></el-step>
-                      <el-step title="未完成"></el-step>
-                      <el-step title="未完成"></el-step>
-                    </el-steps>
-                </el-form>
-                </el-collapse-item>
-              </el-collapse>
-            </el-form-item>
-            <el-form-item label="开始时间">
-              <el-col :span="11">
-                <el-date-picker type="date" placeholder="选择日期"  style="width: 100%;"></el-date-picker>
-              </el-col>
-            </el-form-item>
-            <el-form-item label="部件备注">
-              <el-input type="textarea" ></el-input>
-            </el-form-item>
-          </el-form>
-        </div>
-      </el-main>
-    </el-container>
+    <el-tabs type="border-card">
+      <el-tab-pane label="部件信息">
+        <part-information></part-information>
+      </el-tab-pane>
+      <el-tab-pane label="部件工艺卡">
+        <route-card></route-card>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
+import PartInformation from './PartInformation'
+import RouteCard from './RouteCard'
 export default {
   name: 'ProjectPart',
+  components: {
+    PartInformation,
+    RouteCard
+  },
   data () {
     return {
       date: ''
@@ -77,19 +28,5 @@ export default {
 }
 </script>
 <style scoped>
-  .project-main {
-    margin-top: -20px;
-  }
-  .header {
-    text-align: center;
-    font-size: 24px;
-    line-height: 60px;
-    background: #fff;
-    border-bottom: #eee 1px solid;
-    margin-top: -20px;
-  }
-  .main {
-    position: relative;
-    width: 100%;
-  }
+ 
 </style>
---
+++ src/components/page/market/components/PartInformation.vue
@@ -0,0 +1,89 @@
+<template>
+  <el-container class="project-main">
+      <el-main>
+        <div class="main">
+          <el-form ref="form"  label-width="80px">
+            <el-form-item label="部件名称">
+              <el-col :span="10">
+                <el-input ></el-input>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="截止时间">
+              <el-col :span="10">
+                <el-date-picker type="date" placeholder="选择日期" style="width: 100%;"></el-date-picker>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="部件编码">
+              <el-col :span="10">
+                <el-input ></el-input>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="部件进度">
+              <el-collapse accordion>
+                <el-collapse-item title="子部件1">
+                 <el-form>
+                  <el-form-item label="工艺路线1"></el-form-item>
+                    <el-steps :space="100" :active="1" finish-status="success">
+                      <el-step title="完成"></el-step>
+                      <el-step title="建设中"></el-step>
+                      <el-step title="未完成"></el-step>
+                    </el-steps>
+                  <el-form-item label="工艺路线2"></el-form-item>
+                    <el-steps :space="100" :active="0" finish-status="success">
+                      <el-step title="建设中"></el-step>
+                      <el-step title="未完成"></el-step>
+                      <el-step title="未完成"></el-step>
+                    </el-steps>
+                </el-form>
+                </el-collapse-item>
+                <el-collapse-item title="子部件2">
+                 <el-form>
+                  <el-form-item label="工艺路线1"></el-form-item>
+                    <el-steps :space="100" :active="1" finish-status="success">
+                      <el-step title="完成"></el-step>
+                      <el-step title="建设中"></el-step>
+                      <el-step title="未完成"></el-step>
+                      <el-step title="未完成"></el-step>
+                    </el-steps>
+                </el-form>
+                </el-collapse-item>
+              </el-collapse>
+            </el-form-item>
+            <el-form-item label="开始时间">
+              <el-col :span="11">
+                <el-date-picker type="date" placeholder="选择日期"  style="width: 100%;"></el-date-picker>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="部件备注">
+              <el-input type="textarea" ></el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-main>
+    </el-container>
+</template>
+
+<script>
+export default {
+  name: 'PartInformation'
+}
+</script>
+
+<style scoped>
+ .project-main {
+    margin-top: -20px;
+  }
+  .header {
+    text-align: center;
+    font-size: 24px;
+    line-height: 60px;
+    background: #fff;
+    border-bottom: #eee 1px solid;
+    margin-top: -20px;
+  }
+  .main {
+    position: relative;
+    width: 100%;
+  }
+</style>
+
