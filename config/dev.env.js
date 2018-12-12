01e69371911e875ae785b224519ba99aec8955ff
AIqcwh <35953804+AIqcwh@users.noreply.github.com>
2018-12-12 12:53:18 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://localhost:9096/jmmes/src"'
+  API_ROOT: '"http://localhost:8081/jmmesphp"'
 })
