25d01ac0e636bf728fc7ad398709090992da535f
Lin971018 <30288831+Lin971018@users.noreply.github.com>
2019-03-15 18:35:05 +0800

    2019.3.15-18点33分
    消息通知升级

--- src/components/page/Tabs.vue
+++ src/components/page/Tabs.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="">
-        <div class="crumbs">
-            <!-- <el-breadcrumb separator="/">
+  <div class>
+    <!-- <div class="crumbs">
+            <el-breadcrumb separator="/">
                 <el-breadcrumb-item><i class="el-icon-message"></i> 消息通知</el-breadcrumb-item>
-            </el-breadcrumb> -->
+            </el-breadcrumb>
         </div>
         <div class="container">
             <el-header style="text-align: right; font-size: 12px; height: 20px">
@@ -43,9 +43,6 @@
                                 </template>
                             </el-table-column>
                         </el-table>
-                        <!-- <div class="handle-row">
-                            <el-button type="danger" @click="allDel()">删除全部</el-button>
-                        </div> -->
                     </template>
                 </el-tab-pane>
                 <el-tab-pane :label="`全部消息(${recycle.length})`" name="third">
@@ -57,15 +54,7 @@
                                 </template>
                             </el-table-column>
                             <el-table-column prop="date" width="150"></el-table-column>
-                            <!-- <el-table-column width="120"> -->
-                                <!-- <template slot-scope="scope"> -->
-                                    <!-- <el-button @click="handleRestore(scope.row)">还原</el-button> -->
-                                <!-- </template> -->
-                            <!-- </el-table-column> -->
                         </el-table>
-                        <!-- <div class="handle-row">
-                            <el-button type="danger" @click="emptyTrash()">清空消息</el-button>
-                        </div> -->
                     </template>
                 </el-tab-pane>
             </el-tabs>
@@ -86,171 +75,301 @@
                     <el-checkbox label="销售部"></el-checkbox>
                 </el-checkbox-group>
             </el-dialog>
-        </div> 
-    </div>
+    </div>-->
+    <el-tabs v-model="activeName" type="card">
+      <el-tab-pane :label="`未读消息(${unread.length})`" name="first">
+        <!-- <el-button @click="resetDateFilter">清除日期过滤器</el-button>
+        <el-button @click="clearFilter">清除所有过滤器</el-button>-->
+        <el-table ref="filterTable" :data="unread" style="width: 100%">
+          <!-- <el-table-column prop="name" label="" width="180"></el-table-column> -->
+          <el-table-column prop="address" label="部件信息" :formatter="formatter"></el-table-column>
+          <el-table-column
+            prop="route"
+            label="车间"
+            width="100"
+            :filters="[{ text: 'W车间', value: 'W' }, { text: 'F车间', value: 'F' }, { text: 'G车间', value: 'G' }, { text: 'I车间', value: 'I' }, { text: 'K车间', value: 'K' }, { text: 'S车间', value: 'S' }, { text: 'T车间', value: 'T' }, { text: 'J车间', value: 'J' }, { text: 'L车间', value: 'L' }]"
+            :filter-method="filterRoute"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column
+            prop="tag"
+            label="工序"
+            width="100"
+            :filters="[{ text: '切管', value: '切管' }, { text: '锯床', value: '锯床' }]"
+            :filter-method="filterTag"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column
+            prop="state"
+            label="状态"
+            width="100"
+            :filters="[ { text: '就工', value: '0' },{ text: '完工', value: '1' }]"
+            :filter-method="filterState"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
+          <el-table-column width="120">
+            <template slot-scope="scope">
+              <el-button size="big" type="primary" @click="handleRead(scope.row)">标为已读</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="handle-row">
+          <el-button type="danger" @click="allRead()">全部标为已读</el-button>
+        </div>
+      </el-tab-pane>
+
+      <el-tab-pane :label="`已读消息(${read.length})`" name="second">
+        <el-table ref="filterTable" :data="read" style="width: 100%">
+          <!-- <el-table-column prop="name" label="" width="180"></el-table-column> -->
+          <el-table-column prop="address" label="部件信息" :formatter="formatter"></el-table-column>
+          <el-table-column
+            prop="route"
+            label="车间"
+            width="100"
+            :filters="[{ text: 'W车间', value: 'W' }, { text: 'F车间', value: 'F' }, { text: 'G车间', value: 'G' }, { text: 'I车间', value: 'I' }, { text: 'K车间', value: 'K' }, { text: 'S车间', value: 'S' }, { text: 'T车间', value: 'T' }, { text: 'J车间', value: 'J' }, { text: 'L车间', value: 'L' }]"
+            :filter-method="filterRoute"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column
+            prop="tag"
+            label="工序"
+            width="100"
+            :filters="[{ text: '切管', value: '切管' }, { text: '锯床', value: '锯床' }]"
+            :filter-method="filterTag"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column
+            prop="state"
+            label="状态"
+            width="100"
+            :filters="[ { text: '就工', value: '0' },{ text: '完工', value: '1' }]"
+            :filter-method="filterState"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
+          <el-table-column width="120">
+            <template slot-scope="scope">
+              <el-button size="big" type="danger" @click="handleDel(scope.row)">删除消息</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="handle-row">
+          <el-button type="danger" @click="allDel()">删除全部消息</el-button>
+        </div>
+      </el-tab-pane>
+
+      <el-tab-pane :label="`全部消息(${recycle.length})`" name="third">
+        <el-table ref="filterTable" :data="recycle" style="width: 100%">
+          <!-- <el-table-column prop="name" label="" width="180"></el-table-column> -->
+          <el-table-column prop="address" label="部件信息" :formatter="formatter"></el-table-column>
+          <el-table-column
+            prop="route"
+            label="车间"
+            width="100"
+            :filters="[{ text: 'W车间', value: 'W' }, { text: 'F车间', value: 'F' }, { text: 'G车间', value: 'G' }, { text: 'I车间', value: 'I' }, { text: 'K车间', value: 'K' }, { text: 'S车间', value: 'S' }, { text: 'T车间', value: 'T' }, { text: 'J车间', value: 'J' }, { text: 'L车间', value: 'L' }]"
+            :filter-method="filterRoute"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column
+            prop="tag"
+            label="工序"
+            width="100"
+            :filters="[{ text: '切管', value: '切管' }, { text: '锯床', value: '锯床' }]"
+            :filter-method="filterTag"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column
+            prop="state"
+            label="状态"
+            width="100"
+            :filters="[ { text: '就工', value: '0' },{ text: '完工', value: '1' }]"
+            :filter-method="filterState"
+            filter-placement="bottom-end"
+          ></el-table-column>
+          <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-    export default {
-        name: 'tabs',
-        data() {
-            return {    
-                canupload: false,
-                message: 'first',
-                showHeader: false,
-                checkList: ['全部','车间'],
-                textarea: [],
-                unread: [],
-                read: [],
-                recycle: []
-                }
-        },
-         created () {
-            this.getDataUnread()
-            this.getDataRead()
-            this.getDataRecycle()
-        },
-        methods: {
-             // 获取未读
-            getDataUnread () {
-                var fd = new FormData()
-                fd.append("flag","Unread")
-                var department = localStorage.getItem("ms_department")
-                fd.append("department",department)
-                axios.post(`${this.baseURL}/tabs.php`,fd).then((unread)=> {  //ES6写法
-                    unread = unread.data;
-                    // console.log(unread)
-                    if (unread.success && unread.data) {
-                    this.unread = [];
-                    this.unread = unread.data;
-                    }
+import axios from "axios";
+export default {
+  name: "tabs",
+  data() {
+    return {
+      activeName: "first",
+      canupload: false,
+      //   message: "first",
+      showHeader: false,
+      checkList: ["全部", "车间"],
+      textarea: [],
+      unread: [],
+      read: [],
+      recycle: []
+    };
+  },
+  created() {
+    this.getDataUnread();
+    this.getDataRead();
+    this.getDataRecycle();
+  },
+  methods: {
+    resetDateFilter() {
+      this.$refs.filterTable.clearFilter("date");
+    },
+    clearFilter() {
+      this.$refs.filterTable.clearFilter();
+    },
+    formatter(row, column) {
+      return row.address;
+    },
+    filterTag(value, row) {
+      return row.tag === value;
+    },
+    filterState(value, row) {
+      return row.state === value;
+    },
+    filterRoute(value, row) {
+      //   return row.state === value;
+      if (row.route.indexOf(value) !== -1) {
+        return value;
+      }
+    },
+    filterHandler(value, row, column) {
+      const property = column["property"];
+      return row[property] === value;
+    },
 
-                }); 
-            },
-            getDataInfoSucc (res){
-            // console.log(res.data.rows)
-            res = res.data
-            if(res.success && res.rows){
-                this.rows = res.rows
-                // console.log(this.rows)
-            }
-            },
-            //获取已读
-            getDataRead () {
-                var fd = new FormData()
-                fd.append("flag","Read")
-                var department = localStorage.getItem("ms_department")
-                fd.append("department",department)
-                axios.post(`${this.baseURL}/tabs.php`,fd).then((read)=> {  //ES6写法
-                    read = read.data;
-                    // console.log(read)
-                    if (read.success && read.data) {
-                    this.read = [];
-                    this.read = read.data;
-                    }
-
-                });
-            },
-            //获取回收站
-            getDataRecycle () {
-                var fd = new FormData()
-                fd.append("flag","Recycle")
-                axios.post(`${this.baseURL}/tabs.php`,fd).then((recycle)=> {  //ES6写法
-                    recycle = recycle.data;
-                    // console.log(recycle)
-                    if (recycle.success && recycle.data) {
-                    this.recycle = [];
-                    this.recycle = recycle.data;
-                    }
-
-                });
-            },
-            //进入已读
-            handleRead(row) {
-                console.log(row.id);
-                const item = this.unread.splice(row, 1);
-                // console.log(this.unread);
-                this.read = item.concat(this.read);
-                var fd = new FormData()
-                fd.append("flag","ReadIn")
-                fd.append("id",row.id)
-                axios.post(`${this.baseURL}/tabs.php`,fd).then((recycle)=> {  //ES6写法
-                    recycle = recycle.data;
-
-                });
-            },
-            //全部标记已读
-            allRead(row){
-                for(let i=0;i<this.unread.length;i++){
-                    console.log(this.unread[i].id)
-                    var fd = new FormData()
-                    fd.append("flag","allRead")
-                    fd.append("id",this.unread[i].id)
-                    axios.post(`${this.baseURL}/tabs.php`,fd).then((recycle)=> {  //ES6写法
-                    recycle = recycle.data;
-                    });
-                }
-                const item = this.unread.splice(row);
-                this.read = item.concat(this.read);
-            },
-            //删除
-            handleDel(row) {
-                const item = this.read.splice(row, 1)
-                // this.recycle = item.concat(this.recycle)
-                var fd = new FormData()
-                fd.append("flag","RecycleIn")
-                fd.append("id",row.id)
-                axios.post(`${this.baseURL}/tabs.php`,fd).then((recycle)=> {  //ES6写法
-                recycle = recycle.data;
-
-                });
-            },
-            //删除全部
-            allDel(row) {
-                for(let i=0;i<this.read.length;i++){
-                    console.log(this.read[i].id)
-                    var fd = new FormData()
-                    fd.append("flag","allDel")
-                    fd.append("id",this.read[i].id)
-                    axios.post(`${this.baseURL}/tabs.php`,fd).then((recycle)=> {  //ES6写法
-                    recycle = recycle.data;
-                    });
-                }
-                const item = this.read.splice(row)
-            },
-            //还原
-            handleRestore(row) {
-                const item = this.recycle.splice(row, 1)
-                this.read = item.concat(this.read)
-                var fd = new FormData()
-                fd.append("flag","Reduction")
-                fd.append("id",row.id)
-                axios.post(`${this.baseURL}/tabs.php`,fd).then((reduction)=> {  //ES6写法
-                reduction = reduction.data
-
-                });
-            },
-            //清空回收站
-            emptyTrash(index) {
-                this.recycle.splice(index)
-            }
-        },
-        computed: {
-            unreadNum(){
-                return this.unread.length
-            }
+    // 获取未读
+    getDataUnread() {
+      var fd = new FormData();
+      fd.append("flag", "Unread");
+      var department = localStorage.getItem("ms_department");
+      fd.append("department", department);
+      axios.post(`${this.baseURL}/tabs.php`, fd).then(unread => {
+        //ES6写法
+        unread = unread.data;
+        // console.log(unread.data.length);
+        if (unread.success && unread.data) {
+          this.unread = [];
+          this.unread = unread.data;
         }
+      });
+    },
+    getDataInfoSucc(res) {
+      console.log(res.data.rows)
+      res = res.data;
+      if (res.success && res.rows) {
+        this.rows = res.rows;
+        console.log(this.rows)
+      }
+    },
+    //获取已读
+    getDataRead() {
+      var fd = new FormData();
+      fd.append("flag", "Read");
+      var department = localStorage.getItem("ms_department");
+      fd.append("department", department);
+      axios.post(`${this.baseURL}/tabs.php`, fd).then(read => {
+        //ES6写法
+        read = read.data;
+        // console.log(read)
+        if (read.success && read.data) {
+          this.read = [];
+          this.read = read.data;
+        }
+      });
+    },
+    //获取全部消息
+    getDataRecycle() {
+      var fd = new FormData();
+      fd.append("flag", "Recycle");
+      axios.post(`${this.baseURL}/tabs.php`, fd).then(recycle => {
+        //ES6写法
+        recycle = recycle.data;
+        // console.log(recycle)
+        if (recycle.success && recycle.data) {
+          this.recycle = [];
+          this.recycle = recycle.data;
+        }
+      });
+    },
+    //进入已读
+    handleRead(row) {
+      console.log(row.id);
+      const item = this.unread.splice(row, 1);
+      // console.log(this.unread);
+      this.read = item.concat(this.read);
+      var fd = new FormData();
+      fd.append("flag", "ReadIn");
+      fd.append("id", row.id);
+      axios.post(`${this.baseURL}/tabs.php`, fd).then(recycle => {
+        //ES6写法
+        recycle = recycle.data;
+      });
+    },
+    //全部进入已读
+    allRead(row) {
+      for (let i = 0; i < this.unread.length; i++) {
+        console.log(this.unread[i].id);
+        var fd = new FormData();
+        fd.append("flag", "allRead");
+        fd.append("id", this.unread[i].id);
+        axios.post(`${this.baseURL}/tabs.php`, fd).then(recycle => {
+          //ES6写法
+          recycle = recycle.data;
+        });
+      }
+      const item = this.unread.splice(row);
+      this.read = item.concat(this.read);
+    },
+    //删除
+    handleDel(row) {
+      const item = this.read.splice(row, 1);
+      this.recycle = item.concat(this.recycle);
+      var fd = new FormData();
+      fd.append("flag", "RecycleIn");
+      fd.append("id", row.id);
+      axios.post(`${this.baseURL}/tabs.php`, fd).then(recycle => {
+        //ES6写法
+        recycle = recycle.data;
+      });
+    },
+    //全部删除
+    allDel(row) {
+      for (let i = 0; i < this.read.length; i++) {
+        console.log(this.read[i].id);
+        var fd = new FormData();
+        fd.append("flag", "allDel");
+        fd.append("id", this.read[i].id);
+        axios.post(`${this.baseURL}/tabs.php`, fd).then(recycle => {
+          //ES6写法
+          recycle = recycle.data;
+        });
+      }
+      const item = this.read.splice(row);
     }
-
+  },
+  computed: {
+    unreadNum() {
+      return this.unread.length;
+    }
+  }
+};
 </script>
 
 <style scoped>
-.message-title{
-    cursor: pointer;
+/* div {
+  font-size: 16px;
+} */
+.message-title {
+  cursor: pointer;
 }
-.handle-row{
-    margin-top: 30px;
+.handle-row {
+  margin-top: 30px;
 }
 </style>
 
